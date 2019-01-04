b9d6bcdfc7a76eb9ef7be436b269bcab94ce0dbf
AllanJeremy <aj.dev254@gmail.com>
2019-01-04 14:18:11 +0300

    Updated store model - Added user - Added billing information model file

--- src/models/store.js
+++ src/models/store.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const storeSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
     name: {
         type: String,
         required: true
