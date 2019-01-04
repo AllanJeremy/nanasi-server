6d3b643dc1fd9b0214d70b63b70e391fbffbd5d0
AllanJeremy <aj.dev254@gmail.com>
2019-01-04 13:11:40 +0300

    Added notifications model and config file

---
+++ src/config/notifications.js
@@ -0,0 +1,9 @@
+const NotificationTypes = {
+    Generic: 'generic',
+    Success: 'success',
+    Important: 'important',
+    Info: 'information',
+    Error: 'error'
+};
+
+module.exports = NotificationTypes;
