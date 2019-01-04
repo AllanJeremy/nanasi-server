67d4eb71359cf9c93daddfff380d555fe34df7c6
AllanJeremy <aj.dev254@gmail.com>
2019-01-04 14:42:36 +0300

    Renamed enums to have all uppercase key names as a naming convention - Replaced usage instances of these enums
    - Everytime we see something in pure uppercase, then we can assume it is an enum (unless otherwise stated)

--- src/config/notifications.js
+++ src/config/notifications.js
@@ -1,9 +1,9 @@
 const NotificationTypes = {
-    Generic: 'generic',
-    Success: 'success',
-    Important: 'important',
-    Info: 'information',
-    Error: 'error'
+    GENERIC: 'generic',
+    SUCCESS: 'success',
+    IMPORTANT: 'important',
+    INFO: 'information',
+    ERROR: 'error'
 };
 
 module.exports = NotificationTypes;
