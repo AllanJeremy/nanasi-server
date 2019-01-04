617785dc0c2afdc800a6ff440ae611af84dd3d2b
AllanJeremy <aj.dev254@gmail.com>
2019-01-04 14:07:36 +0300

    Added address model

---
+++ src/models/users/address.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const addressSchema = new mongoose.Schema({
+    addressLine1:{
+        type: String,
+        required: true
+    },
+    addressLine2: String,
+    country: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Country'
+    },
+    city:{
+        type: String,
+        required: true
+    },
+    state: { // State or province
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'State'
+    },
+    zipCode: String,
+});
+
+// Exports
+module.exports = mongoose.model('Address',addressSchema);
