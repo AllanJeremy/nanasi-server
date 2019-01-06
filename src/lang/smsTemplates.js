80072d84d90a7896f3f3f4186ae9368626d48b5c
AllanJeremy <aj.dev254@gmail.com>
2019-01-07 01:26:13 +0300

    Updating user and adding otp on register user works

--- src/lang/smsTemplates.js
+++ src/lang/smsTemplates.js
@@ -1,6 +1,6 @@
 // Send otp
-module.exports.sendOtp = (otp, lang) => {
-    const message = `Your Nanasi OTP is ${otp}`;
+module.exports.sendOtp = (otp, otpType) => {
+    const message = `Your Nanasi ${otpType} OTP is ${otp}`;
 
     //TODO: Possibly add translation
     return message;
--- src/lib/messaging.js
+++ src/lib/messaging.js
@@ -1,7 +1,37 @@
 const AfricasTalking = require('../config/africasTalking');
-const sms = AfricasTalking.sms;
+const sms = AfricasTalking.SMS;
 
+const SENDER_ID = 'Nanasi.co';
+module.exports.messageStatusCode = {
+    PROCESSED: '100',
+    SENT: '101',
+    QUEUED: '102',
+    RISK_HOLD: '401',
+    INVALID_SENDER_ID: '402',
+    INVALID_PHONE_NUMBER: '403',
+    UNSUPPORTED_NUMBER_TYPE: '404',
+    INSUFFICIENT_BALANCE: '405',
+    USER_IN_BLACK_LIST: '406',
+    COULD_NOT_ROUTE: '407',
+    INTERNAL_SERVER_ERROR: '500',
+    GATEWAY_ERROR: '501',
+    REJECTED_BY_GATEWAY: '502'
+};
 // This allows us to send SMSes
-module.exports.sendSms = (recipients,message)=>{
+module.exports.sendSms = (recipients, message, callback) => {
+    const options = {
+        to: recipients,
+        message: message,
+        //TODO: Add sender id once we have it
+    };
 
+    const sendMessage = sms.send(options);
+
+    sendMessage.then(response => {
+        console.log(response);
+        return callback(response);
+    }).catch(err => {
+        console.error(err);
+        return err;
+    });
 };
