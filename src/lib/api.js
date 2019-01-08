a6c6b26b097728e260bf5a62a8daddc69e04ff1c
AllanJeremy <aj.dev254@gmail.com>
2019-01-08 11:09:27 +0300

    Added status code option to API response & getError

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,13 +1,15 @@
-module.exports.getResponse = (isOk, message, data) => {
+module.exports.getResponse = (isOk, message, data, statusCode) => {
     let response = {};
     response.ok = isOk;
+    response.status = statusCode || 200;
     response.message = message;
-    response.data = data;
+    response.data = data || undefined;
     return response;
 };
 
-module.exports.getError = (message, error) => {
-    let response = this.getResponse(false, message);
+module.exports.getError = (message, error, statusCode) => {
+    statusCode = statusCode || 500;
+    let response = this.getResponse(false, message, null, statusCode);
     response.error = error;
     return response;
 };
