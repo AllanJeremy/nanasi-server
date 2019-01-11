83c965b867ad63c01e5b2d3fc81527c3bc35d176
AllanJeremy <aj.dev254@gmail.com>
2019-01-11 11:27:45 +0300

    Implemented the cart routes - TODO: Add middleware and validation

--- src/modules/cart/cart.js
+++ src/modules/cart/cart.js
@@ -1,1 +1,80 @@
  const Cart = require('../../models/cart');
+
+ // Get multiple products by filter
+ function _getProductsByFilter(filter, callback) {
+     filter = filter || {};
+     return Product.find(filter)
+         .populate('store', '_id name')
+         .populate('images')
+         .populate('variants')
+         .then((productsFound) => {
+             const productCount = productsFound.length;
+             const isOk = (productCount > 0);
+             const statusCode = isOk ? 200 : 404;
+             const message = isOk ? FeedbackMessages.itemsFoundWithCount(productsFound, 'Products') : FeedbackMessages.itemNotFound('Products');
+
+             return callback(
+                 Api.getResponse(isOk, message, {
+                     count: productCount,
+                     products: productsFound
+                 }, statusCode)
+             );
+         })
+         .catch(err => {
+             return callback(
+                 Api.getError(FeedbackMessages.operationFailed('get products'), err)
+             );
+         });
+ }
+
+ // Get product by filter
+ function _getSingleProductByFilter(filter, callback) {
+     return Product.findOne(filter)
+         .populate('store', '_id name')
+         .populate('images')
+         .populate('variants')
+         .then((productFound) => {
+             const isOk = productFound ? true : false;
+             const statusCode = isOk ? 200 : 404;
+             const message = isOk ? FeedbackMessages.itemsFound('Product') : FeedbackMessages.itemNotFound('Product');
+
+             return callback(
+                 Api.getResponse(isOk, message, {
+                     product: productFound
+                 }, statusCode)
+             );
+         })
+         .catch(err => {
+             return callback(
+                 Api.getError(FeedbackMessages.operationFailed('get products'), err)
+             );
+         });
+ }
+
+ /* 
+     EXPORTS
+ */
+ // Add item to cart
+ module.exports.addCartItem = (itemData, callback) => {
+
+ };
+
+ // View cart items for the current user
+ module.exports.getUserCart = (userId, callback) => {
+
+ };
+
+ // Get single cart item
+ module.exports.getCartItem = (cartItemId, callback) => {
+
+ };
+
+ // Update cart item ~ Often used to update the item quantity in the cart
+ module.exports.updateCartItem = (cartItemId, updateData, callback) => {
+
+ };
+
+ // Delete cart item ~ Remove item from cart
+ module.exports.deleteCartItem = (itemData, callback) => {
+
+ };
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,56 +1,54 @@
 const express = require('express');
 const router = express.Router();
 
+const cart = require('../modules/cart/cart');
+
 /* CART ENDPOINTS */
 // Add item to cart
-router.post('/',(req,res,next)=>{
-    res.status(201);
-
-    res.json({
-        message: `Creating cart item`
+//* Buyer accessible
+router.post('/', (req, res, next) => {
+    //TODO: Set the user id to the current user id, passed via middleware
+    //req.body.data.user = req.userData._id;
+    cart.addCartItem(req.body.data, response => {
+        return res.status(response.statusCode).json(response);
     });
 });
 
 // View cart items for the current user
-router.get('/',(req,res,next)=>{
-    res.status(200);
-
-    res.json({
-        message: `Get cart items belonging to the current user`
+//* Buyer accessible
+router.get('/', (req, res, next) => {
+    //TODO: Set the user id to the current user id, passed via middleware
+    cart.getUserCart(req.userData._id, response => {
+        return res.status(response.statusCode).json(response);
     });
 });
 
 
 // Get single cart item
-router.get('/:cartItemId',(req,res,next)=>{
-    res.status(200);
-
-    const cartItemId = req.params.cartItemId;
-    res.json({
-        id: cartItemId,
-        message: `Get cart item with the id of ${cartItemId}`
+//* Buyer accessible
+router.get('/:cartItemId', (req, res, next) => {
+    //TODO: Set the user id to the current user id, passed via middleware
+    //TODO: Check if the buyer owns this cart item before allowing updates
+    cart.getCartItem(req.params.cartItemId, response => {
+        return res.status(response.statusCode).json(response);
     });
 });
 
 // Update cart item ~ Often used to update the item quantity in the cart
-router.patch('/:cartItemId',(req,res,next)=>{
-    res.status(200);
-
-    const cartItemId = req.params.cartItemId;
-    res.json({
-        id: cartItemId,
-        message: `Updating cart item with the id of ${cartItemId}`
+//* Buyer accessible
+router.patch('/:cartItemId', (req, res, next) => {
+    //TODO: Check if the buyer owns this cart item before allowing updates
+    cart.updateCartItem(req.params.cartItemId, req.body.data, response => {
+        return res.status(response.statusCode).json(response);
     });
 });
 
 // Delete cart item ~ Remove item from cart
-router.get('/:cartItemId',(req,res,next)=>{
-    res.status(200);
-
-    const cartItemId = req.params.cartItemId;
-    res.json({
-        id: cartItemId,
-        message: `Get cart item with the id of ${cartItemId}`
+//* Buyer accessible
+router.get('/:cartItemId', (req, res, next) => {
+    //TODO: Check if the buyer owns this cart item before allowing delete
+    cart.deleteCartItem(req.params.cartItemId, response => {
+        return res.status(response.statusCode).json(response);
     });
 });
 
