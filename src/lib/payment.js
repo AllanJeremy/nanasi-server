901cc70638393041d11fe3febb0abb27c2844206
AllanJeremy <aj.dev254@gmail.com>
2019-01-14 22:31:35 +0300

    Payment lib base - TODO: Add the Africa's talking implementation

---
+++ src/lib/payment.js
@@ -0,0 +1,12 @@
+const AfricasTalking = require('../config/africasTalking');
+const Payments = AfricasTalking.PAYMENTS;
+
+// Checkout
+module.exports.checkout = (phone, amount, currencyCode, meta) => {
+
+};
+
+// Withdraw funds ~ Used by merchants
+module.exports.withdraw = () => {
+    //TODO: Add implementation
+};
