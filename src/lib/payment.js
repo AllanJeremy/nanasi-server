01d8ab96044ed923c3e23c65fe04532fb6047dac
AllanJeremy <aj.dev254@gmail.com>
2019-01-15 10:35:24 +0300

    Implemented checkout in payment library

--- src/lib/payment.js
+++ src/lib/payment.js
@@ -2,8 +2,23 @@ const AfricasTalking = require('../config/africasTalking');
 const Payments = AfricasTalking.PAYMENTS;
 
 // Checkout
-module.exports.checkout = (phone, amount, currencyCode, meta) => {
+module.exports.checkout = (phone, amount, storeId, currencyCode, meta) => {
+    currencyCode = currencyCode || 'KES';
 
+    const options = {
+        productName: storeId,
+        phoneNumber: phone,
+        currencyCode: currencyCode,
+        amount: amount,
+        metadata: meta
+    };
+
+    Payments.mobileCheckout(options)
+        .then(response => {
+            console.log(response);
+        }).catch(err => {
+            console.log(err);
+        });
 };
 
 // Withdraw funds ~ Used by merchants
