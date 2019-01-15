8fd12a1c1a615639105ebd398d640548248c5d72
AllanJeremy <aj.dev254@gmail.com>
2019-01-15 14:06:30 +0300

    Updated Nanasi config file

--- src/config/nanasi.js
+++ src/config/nanasi.js
@@ -1,2 +1,10 @@
 // Nanasi's commission in percentage format
-module.exports.nanasiPercentage = 8.0;
+module.exports.rates = {
+    NANASI_PERCENTAGE: 8.0
+};
+
+// Product names used by Africa's talking
+module.exports.products = {
+    HOLDINGS: 'NanasiHoldings',
+    STORES: 'NanasiStores'
+};
