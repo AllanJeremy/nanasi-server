559b1df3938e6a9fcb159fe7d7b4850c64d4e80e
AllanJeremy <aj.dev254@gmail.com>
2019-02-01 04:38:27 +0300

    Removed _id from image schema - This allows us to auto-generate the _id

--- src/models/image.js
+++ src/models/image.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const imageSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
     imageUrl: String,
     thumbnailUrl: String,
     altText: String,
