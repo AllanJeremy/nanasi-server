0d13952d9ae080d9367749d325e51976824e9314
AllanJeremy <aj.dev254@gmail.com>
2019-02-03 10:33:27 +0300

    Updated cart schema - Added DateAdded - Added items & made it into an array containing cart items - Added orderIsCompleted

--- src/models/cart.js
+++ src/models/cart.js
@@ -4,18 +4,28 @@ require('./products/product');
 require('./users/user');
 
 const cartSchema = new mongoose.Schema({
-    product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Product',
-        required: true
-    },
-    quantity: {
-        type: Number,
-        required: true
-    },
+    items: [{
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product',
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: true
+        },
+        dateAdded: {
+            type: Date,
+            default: Date.now()
+        }
+    }],
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
+    },
+    orderIsCompleted: { // Used to detect abandoned carts
+        type: Boolean,
+        default: false
     }
 });
 
