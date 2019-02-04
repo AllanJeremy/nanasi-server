846e2577732c5fc6b807b17677249a738d9c15de
AllanJeremy <aj.dev254@gmail.com>
2019-02-04 12:09:47 +0300

    Fixed issue with user model

--- src/models/users/user.js
+++ src/models/users/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const moment = require('moment');
 
-require('./users/address');
+require('./address');
 
 const userSchema = new mongoose.Schema({
     firstName: {
