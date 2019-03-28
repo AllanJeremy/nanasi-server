3fcd1b3342a6ccacddf99ee3fb3439fc70f4ee5c
AllanJeremy <aj.dev254@gmail.com>
2019-03-28 13:14:18 +0300

    Removed AT config from the nanasi config file - These settings will now reside in the Africa's Talking config file as they are specific to Africa's Talking and not exactly Nanasi

--- src/config/nanasi.js
+++ src/config/nanasi.js
@@ -2,9 +2,3 @@
 module.exports.rates = {
     NANASI_PERCENTAGE: 8.0
 };
-
-// Product names used by Africa"s talking
-module.exports.products = {
-    HOLDINGS: "NanasiHoldings", // Holds Nanasi funds
-    STORES: "NanasiStores" // Holds stores funds. These are funds that belong to the stores
-};
