2b4dd96803a85f29e5b71fffdb0e18c587823d47
AllanJeremy <aj.dev254@gmail.com>
2019-04-03 17:20:50 +0300

    Added notification middleware - Checking for authorization before allowing access to notifications - Ownership check in respective endpoints

--- src/routes/notifications.js
+++ src/routes/notifications.js
@@ -2,14 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const notification = require("../modules/notifications/notifications");
+const CheckAuth = require("../middleware/checkAuth");
+const Ownership = require("../middleware/entityOwnership");
 
 /* NOTIFICATION ENDPOINTS */
 // Create notification
-//* Globally accessible
-router.post("/", (req, res, next) => { //TODO: Add db code
-
-    //TODO: Set the user id to the current user id, passed via middleware
-    // req.body.data.user = req.userData._id;
+//* Logged in user accessible
+router.post("/", CheckAuth.userLoggedIn, (req, res, next) => {
+    req.body.data.user = req.userData.id;
     notification.createNotification(req.body.data, (response) => {
         return res.status(response.statusCode).json(response);
     });
@@ -17,36 +17,32 @@ router.post("/", (req, res, next) => { //TODO: Add db code
 
 // View user notifications for the currently logged in user
 //* Logged in user accessible
-router.get("/", (req, res, next) => { //TODO: Add db code
-    //TODO: Set the user id to the current user id, passed via middleware
-    notification.getUserNotifications(req.userData._id, (response) => {
+router.get("/", CheckAuth.userLoggedIn, (req, res, next) => {
+    notification.getUserNotifications(req.userData.id, (response) => {
         return res.status(response.statusCode).json(response);
     });
 });
 
 // Mark all current user notifications as read
 //* Logged in user accessible
-router.patch("/mark-all-read", (req, res, next) => { //TODO: Add db code
-    //TODO: Set the user id to the current user id, passed via middleware
-    notification.markUserNotificationsAsRead(req.userData._id, (response) => {
+router.patch("/mark-all-read", CheckAuth.userLoggedIn, (req, res, next) => {
+    notification.markUserNotificationsAsRead(req.userData.id, (response) => {
         return res.status(response.statusCode).json(response);
     });
 });
 
 // Mark notification that has the if of `notificationId` as read
 //* Logged in user accessible
-router.patch("/mark-read/:notificationId", (req, res, next) => { //TODO: Add db code
-    //TODO: Set the user id to the current user id, passed via middleware
-    notification.markNotificationAsRead(req.userData._id, (response) => {
+router.patch("/mark-read/:notificationId", CheckAuth.userLoggedIn, Ownership.notificationBelongsToUser, (req, res, next) => {
+    notification.markNotificationAsRead(req.userData.id, (response) => {
         return res.status(response.statusCode).json(response);
     });
 });
 
 // Mark notification as unread
 //* Logged in user accessible
-router.patch("/mark-unread/:notificationId", (req, res, next) => { //TODO: Add db code
-    //TODO: Set the user id to the current user id, passed via middleware
-    notification.markNotificationAsUnread(req.userData._id, (response) => {
+router.patch("/mark-unread/:notificationId", CheckAuth.userLoggedIn, Ownership.notificationBelongsToUser, (req, res, next) => {
+    notification.markNotificationAsUnread(req.userData.id, (response) => {
         return res.status(response.statusCode).json(response);
     });
 });
