476950ddb3acf606d0ff83b1752e657b3fefa714
AllanJeremy <aj.dev254@gmail.com>
2019-04-06 19:22:12 +0300

    Comment update in order model for extra clarification - User represents buyer that ordered product

--- src/models/orders/order.js
+++ src/models/orders/order.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
-    // User that ordered the product
+    // Buyer that ordered the product
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
