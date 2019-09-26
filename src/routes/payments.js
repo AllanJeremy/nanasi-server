57d47a5e7a04ef18537a08933aa48013845ff6e0
AllanJeremy <aj.dev254@gmail.com>
2019-09-26 11:18:55 +0300

    Added shell for withdraw payment functionality

--- src/routes/payments.js
+++ src/routes/payments.js
@@ -12,4 +12,50 @@ router.post("/checkout", CheckAuth.buyerLoggedIn, (req, res, next) => {
     });
 });
 
+// Withdraw funds
+router.post("/withdraw", CheckAuth.merchantLoggedIn, (req, res, next) => {
+    // Enter amount
+    // Billing id
+
+    /* 
+        response:
+        {
+            ok: true
+            message: "Successfully withdrew ${amount} through ${paymentMethod}",
+            data: {
+                amount: ,
+                billing:{
+                    paymentMethod: ,
+                    details: {
+
+                    }
+                } 
+                date: ${date}
+                status: "success|pending|failed"
+                message: "Successful | Pending | (Insufficient funds | Technical error)"
+            }
+        }
+    */
+});
+
+// Get the current merchant's withdrawals
+router.get("/withdrawals", CheckAuth.merchantLoggedIn, (req, res, next) => {
+    /* 
+      data: {
+          count: x,
+          totalValue: x,
+          withdrawals:[{
+              billing: {
+                  paymentMethod:
+                  details: 
+              },
+              amount: x,
+              date: x,
+              status: "success|pending|failed"
+    }]
+      }
+    */
+});
+
+
 module.exports = router;
